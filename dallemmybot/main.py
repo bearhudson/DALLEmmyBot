0540184558c343125cf8fd4bb26f0a078f7cada4
bhudson <brian.hudson@flexential.com>
2023-11-22 23:07:20 -0500

    Adding more output and randomness

--- dallemmybot/main.py
+++ dallemmybot/main.py
@@ -1,5 +1,6 @@
 import urllib.request
 import urllib.parse
+from random import choice
 import requests
 from PIL import UnidentifiedImageError
 from PIL import Image
@@ -8,6 +9,7 @@ import openai
 import os
 from griptape.structures import Agent
 import time
+from shot_type import shot_type_list, lens_type
 
 
 def generate(client, text):
@@ -24,24 +26,30 @@ def generate(client, text):
 
 
 def main():
-    t_stamp = int(time.time())
-    client = openai.OpenAI(
-        api_key=os.getenv("OPENAI_API_KEY")
-    )
-    gt_agent = Agent()
-    r_topic = requests.get('https://en.wikipedia.org/wiki/Special:Random')
-    decoded_url = urllib.parse.unquote(r_topic.url)
-    topic_index = decoded_url.rfind('/')
-    r_topic_str = decoded_url[topic_index + 1:].replace('_', ' ')
-    item_description = gt_agent.run(f"Tell me some brief details about {r_topic_str}.")
-    full_text = gt_agent.run(f"Write me a sentence about {r_topic_str} from the perspective of a director writing a "
-                             f"movie scene at a random time in the day and include as much detail as possible "
-                             f"about the colors, angles, subjects and objects in the scene but do not use "
-                             f"the word camera and keep the view static and obey the rule of thirds including taking"
-                             f"into consideration the following details -- {item_description}.")
-    print(full_text.output_task)
-    url1 = generate(client, str(full_text.output_task))
-    response = requests.get(url1)
+    try:
+        t_stamp = int(time.time())
+        client = openai.OpenAI(
+            api_key=os.getenv("OPENAI_API_KEY")
+        )
+        gt_agent = Agent()
+        r_topic = requests.get('https://en.wikipedia.org/wiki/Special:Random')
+        decoded_url = urllib.parse.unquote(r_topic.url)
+        topic_index = decoded_url.rfind('/')
+        r_topic_str = decoded_url[topic_index + 1:].replace('_', ' ')
+        item_description_full = gt_agent.run(f"Tell me some brief details about {r_topic_str}.")
+        item_description = item_description_full.output_task.output
+        full_text = gt_agent.run(f"Write me a brief paragraph description about {r_topic_str} "
+                                 f"with a {choice(shot_type_list)}, and {choice(lens_type)} style lens, "
+                                 f"describe the scene and relative, local art style from the perspective of a painter "
+                                 f"painting a movie scene at a specific, random time in the day and include as much "
+                                 f"detail as possible about the colors, describe the subjects and objects in the scene "
+                                 f"but do not use the word camera and keep the view static and obey the rule of thirds "
+                                 f"including taking into consideration the following details -- {item_description.value}.")
+        url1 = generate(client, str(full_text.output_task))
+        response = requests.get(url1)
+    except openai.BadRequestError:
+        print("Error generating prompt")
+        exit(1)
     try:
         text_file = f"./output/{t_stamp}-{r_topic_str}.txt"
         image_file = f"./output/{t_stamp}-{r_topic_str}.png"
@@ -50,9 +58,10 @@ def main():
         png_file = Image.open(img_file)
         png_file.save(image_file)
         with open(text_file, 'w') as file:
-            file.write(str(full_text.output_task))
+            file.write(f"{item_description.value}\n\n{str(full_text.output_task)}")
     except UnidentifiedImageError:
         print("Error: Unable to identify image format")
+        exit(1)
 
 
 if __name__ == "__main__":
---
+++ dallemmybot/shot_type.py
@@ -0,0 +1,30 @@
+shot_type_list = [
+    "close-up shot",
+    "medium shot",
+    "long shot",
+    "close up shot",
+    "extreme close up shot",
+    "extreme long shot",
+    "cowboy shot",
+    "over the shoulder shot",
+    "pov shot",
+    "establishing shot",
+    "low angle shot",
+    "ground level shot",
+    "knee level shot",
+    "hip level shot",
+    "eye level shot",
+    "high angle shot",
+    "bird's eye view shot",
+    "aerial shot",
+    "dutch camera angle shot",
+]
+
+lens_type = [
+    "telephoto",
+    "wide angle",
+    "portrait",
+    "prime",
+    "zoom",
+    "mid length"
+]
